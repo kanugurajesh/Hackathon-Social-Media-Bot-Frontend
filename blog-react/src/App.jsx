3cd960263d18fc1acb8ec85ea6770513767b3249
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-14 23:02:09 +0530

    adding all the data

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
+import { useState, useEffect } from 'react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import styled from 'styled-components'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -15,16 +16,51 @@ const StyledDiv = styled.div`
   gap: 1rem;
 `
 
-const App = () => (
-  <>
-    <StyledDiv>
-      <AccountCircleIcon sx={{ fontSize: 100 }} />
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={['google', 'github']}
-      />
-    </StyledDiv>
-  </>
-)
-export default App
+// const App = () => (
+//   <>
+//     <StyledDiv>
+//       <AccountCircleIcon sx={{ fontSize: 100 }} />
+//       <Auth
+//         supabaseClient={supabase}
+//         appearance={{ theme: ThemeSupa }}
+//         providers={['google', 'github']}
+//       />
+//     </StyledDiv>
+//   </>
+// )
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <>
+        <StyledDiv>
+          <AccountCircleIcon sx={{ fontSize: 100 }} />
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={['google', 'github']}
+          />
+      </StyledDiv>
+      </>
+    )
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+}
