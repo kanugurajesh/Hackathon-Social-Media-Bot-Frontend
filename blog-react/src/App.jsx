e0bab7230e2096247f7577724055afa501b79270
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-14 23:19:23 +0530

    adding all the data

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import styled from 'styled-components'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ResponsiveAppBar from './components/header'
 const supabase = createClient('https://cyfpjgfjgocvjoqdblbp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZnBqZ2ZqZ29jdmpvcWRibGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0OTIzNTIsImV4cCI6MjAwNDA2ODM1Mn0.3hq1S-yAh1Uq2K5lBbYJSvvmvGlg8NHd0kq7P-rlwQs')
+
 
 // create a styled component to make the items in in it display column
 const StyledDiv = styled.div`
@@ -15,19 +17,6 @@ const StyledDiv = styled.div`
   height: 100vh;
   gap: 1rem;
 `
-
-// const App = () => (
-//   <>
-//     <StyledDiv>
-//       <AccountCircleIcon sx={{ fontSize: 100 }} />
-//       <Auth
-//         supabaseClient={supabase}
-//         appearance={{ theme: ThemeSupa }}
-//         providers={['google', 'github']}
-//       />
-//     </StyledDiv>
-//   </>
-// )
 
 export default function App() {
   const [session, setSession] = useState(null)
@@ -61,6 +50,8 @@ export default function App() {
     )
   }
   else {
-    return (<div>Logged in!</div>)
+    return (
+      <ResponsiveAppBar />
+    )
   }
 }
