a6eedf20f224cfa3cd2459556be9d230de3a696f
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-15 01:50:55 +0530

    adding all the data

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -4,10 +4,10 @@ import { useState, useEffect } from 'react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import styled from 'styled-components'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ThemedExample from './components/Chat'
+import Header from './components/Header';
+import Footer from './components/Footer';
 const supabase = createClient('https://cyfpjgfjgocvjoqdblbp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZnBqZ2ZqZ29jdmpvcWRibGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0OTIzNTIsImV4cCI6MjAwNDA2ODM1Mn0.3hq1S-yAh1Uq2K5lBbYJSvvmvGlg8NHd0kq7P-rlwQs')
 
-// create a styled component to make the items in in it display column
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,9 +17,28 @@ const StyledDiv = styled.div`
   gap: 1rem;
 `
 
-export default function App() {
-  const [session, setSession] = useState(null)
+const StyledChat = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100vh;
+  width: 100vw;
+  gap: 1rem;
+`
 
+// style footer
+const StyledFooter = styled.div`
+width:95vw;
+padding: 1rem;
+`
+
+export default function App() {
+  const [session, setSession] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => setShow(!show);
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
@@ -51,7 +70,12 @@ export default function App() {
   else {
     return (
       <>
-        <ThemedExample />
+        <StyledChat>
+        <Header />
+        <StyledFooter>
+        <Footer />
+        </StyledFooter>
+        </StyledChat>
       </>
     )
   }
