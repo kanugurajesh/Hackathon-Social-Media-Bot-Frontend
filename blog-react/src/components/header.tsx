7179c1800e48ad46b5c5ac9dd9e36f3c1e39a685
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-14 23:20:42 +0530

    adding all the data

--- blog-react/src/components/header.tsx
+++ blog-react/src/components/header.tsx
@@ -7,7 +7,8 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+// import Avatar from '@mui/material/Avatar';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
@@ -129,7 +130,9 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <AccountCircleIcon sx={{ fontSize: '2.5rem' }} />
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+               */}
               </IconButton>
             </Tooltip>
             <Menu
