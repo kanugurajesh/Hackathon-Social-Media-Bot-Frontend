972191abbf5f35df6f370534b0b2b0ca399eb0da
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-15 03:01:30 +0530

    adding all the data

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -5,7 +5,11 @@ import { ThemeSupa } from '@supabase/auth-ui-shared'
 import styled from 'styled-components'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import React from 'react';
+import { Input } from 'antd';
+const { TextArea } = Input;
 const supabase = createClient('https://cyfpjgfjgocvjoqdblbp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZnBqZ2ZqZ29jdmpvcWRibGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0OTIzNTIsImV4cCI6MjAwNDA2ODM1Mn0.3hq1S-yAh1Uq2K5lBbYJSvvmvGlg8NHd0kq7P-rlwQs')
 
 const StyledDiv = styled.div`
@@ -28,15 +32,34 @@ const StyledChat = styled.div`
   gap: 1rem;
 `
 
+const StyledMesseges = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 100vh;
+  width: 100vw;
+  gap: 1rem;
+`
+
 // style footer
 const StyledFooter = styled.div`
-width:95vw;
-padding: 1rem;
+  width:95vw;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-bottom: 1rem;
 `
 
 export default function App() {
   const [session, setSession] = useState(null);
   const [show, setShow] = useState(false);
+  // create a list of alerts
+  const [alerts, setAlerts] = useState(["who is dhoni", "who cpu work", "what is programming", "hello who are you",]);
+  // create a list of messages
+  const [messages, setMessages] = useState(["he is great","cpu is a brain of computer","programming is a art","i am a bot"]);
 
   const handleClick = () => setShow(!show);
   useEffect(() => {
@@ -52,6 +75,16 @@ export default function App() {
 
     return () => subscription.unsubscribe()
   }, [])
+
+  const requestSendner = async (e) => {
+    e.preventDefault();
+    // update the alerts
+    setAlerts([...alerts, e.target.value]);
+    // update the messages
+    setMessages([...messages, ""]);
+    console.log(e.target.value);
+    // send the request to the server
+  }
 
   if (!session) {
     return (
@@ -72,8 +105,22 @@ export default function App() {
       <>
         <StyledChat>
         <Header />
+        <StyledMesseges>
+        {alerts.map((alert, index) => (
+          <React.Fragment key={index}>
+            <Alert severity="info" sx={{ width: '45vw', alignSelf: 'flex-start' }}>{alert}</Alert>
+            {messages[index] && (
+              <Alert severity="success" sx={{ width: '45vw', alignSelf: 'flex-end' }}>{messages[index]}</Alert>
+            )}
+          </React.Fragment>
+        ))}
+        </StyledMesseges>
         <StyledFooter>
-        <Footer />
+        {/* <Search placeholder="input search text" enterButton="Search" size="large" /> */}
+        {/* <StyledInput> */}
+        <TextArea rows={3} placeholder="maxLength is 6" maxLength={100} />
+        <Button variant="outlined">Outlined</Button>
+        {/* </StyledInput> */}
         </StyledFooter>
         </StyledChat>
       </>
--- blog-react/src/components/Footer.tsx
+++ blog-react/src/components/Footer.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { Input } from 'antd';
+// import React from 'react';
+// import { Input } from 'antd';
 
-const { Search } = Input;
+// const { TextArea } = Input;
 
-const App: React.FC = () => (
-  <>
-    {/* <Search placeholder="input search loading default" loading />
-    <br />
-    <br />
-    <Search placeholder="input search loading with enterButton" loading enterButton />
-    <br />
-    <br /> */}
-    <Search placeholder="input search text" enterButton="Search" size="large" loading />
-  </>
-);
+// const App: React.FC = () => (
+//   <>
+//     <TextArea rows={4} />
+//     <br />
+//     <br />
+//     <TextArea rows={4} placeholder="maxLength is 6" maxLength={6} />
+//   </>
+// );
 
-export default App;
+// export default App;
