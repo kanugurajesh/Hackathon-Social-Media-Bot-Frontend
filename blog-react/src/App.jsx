b4094453816e2e3346646fbea2f56721c9fa2d99
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-14 20:32:51 +0530

    adding all the data

--- blog-react/src/App.jsx
+++ blog-react/src/App.jsx
@@ -1,13 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import Avatar from '@mui/material/Avatar';
+
 const supabase = createClient('https://cyfpjgfjgocvjoqdblbp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZnBqZ2ZqZ29jdmpvcWRibGJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0OTIzNTIsImV4cCI6MjAwNDA2ODM1Mn0.3hq1S-yAh1Uq2K5lBbYJSvvmvGlg8NHd0kq7P-rlwQs')
 
 const App = () => (
-  <Auth
-    supabaseClient={supabase}
-    appearance={{ theme: ThemeSupa }}
-    providers={['google', 'github']}
-  />
+  <>
+    <Avatar alt="Remy Sharp" src="./automate.png" />
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      providers={['google', 'github']}
+    />
+  </>
 )
 export default App
