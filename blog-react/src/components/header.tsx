3bd204b489115a7f16f9b4312f1d051cfa7c1fbe
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-15 05:57:10 +0530

    adding all the data

--- blog-react/src/components/header.tsx
+++ blog-react/src/components/header.tsx
@@ -57,7 +57,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Gmate
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -113,7 +113,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Gmate
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
